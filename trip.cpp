975f712a8e08f1807e36ce75807170763548f189
Nohil1999 <48293786+Nohil1999@users.noreply.github.com>
2019-08-09 21:14:21 +0530

    Add files via upload

---
+++ trip.cpp
@@ -0,0 +1,68 @@
+
+
+
+Skip to content
+Using Gmail with screen readers
+Enable desktop notifications for Gmail.
+   OK  No thanks
+
+4 of 1,047
+trip
+Inbox
+x
+
+Soham Joshi <sohamsjoshi98@gmail.com>
+Attachments
+12:52 AM (45 minutes ago)
+to me
+
+
+Attachments area
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+
+ int students, counter=0;
+ double payments[1000], total=0, deficits[1000], surplus[1000];
+
+ while(1)
+ {
+ cin >> students;
+ total = 0;
+ if (students == 0)
+ {
+ for (int i=0; i<counter; i++)
+ {
+ if (deficits[i] < surplus[i]) deficits[i] = surplus[i];
+
+ cout << '$' << setprecision(2) << fixed << deficits[i] << endl;
+ }
+
+ return 0;
+ }
+
+ for (int i=0; i<students; i++) {cin >> payments[i]; total+= payments[i];}
+
+ total /= students;
+
+ deficits[counter] = 0;
+ surplus[counter] = 0;
+ for (int i=0; i<students; i++)
+ if (payments[i] < total) deficits[counter] += (floor((total - payments[i])*100))/100;
+ else if (payments[i] > total) surplus[counter] += (floor((-total + payments[i])*100))/100;
+
+ counter++;
+ }
+
+ return 0;
+}
+trip.cpp
+Displaying trip.cpp.
